88faed54311a5311b23b78eac65b3c49bee8d266
Erzp10 <86994280+Erzp10@users.noreply.github.com>
2021-07-24 00:18:27 -0600

    Avance Eduardo, agregue las pantallas de Login, Crear Usuario, Menu Lateral

---
+++ CarWashCatrachoHN/CarWashCatrachoHN/AppShell.xaml.cs
@@ -0,0 +1,28 @@
+﻿using CarWashCatrachoHN.ViewModels;
+using CarWashCatrachoHN.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CarWashCatrachoHN
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        private void btnFacturacion_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
